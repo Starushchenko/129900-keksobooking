35809af941ff273656883c8b8190a82279afaeaf
Yaroslav Starushchenko <starushchenkoyaroslav@gmail.com>
2017-04-19 23:07:13 +0300

    Drag для главного пина. Функционал не совершенен, так как пин может вылезти за пределы карты

--- js/map.js
+++ js/map.js
@@ -9,3 +9,42 @@ window.pins.renderPins(pinMap, window.data.createAdsCollection(), function (pind
     window.pins.setPinInactive();
   });
 });
+
+var pinHandle = document.querySelector('.pin__main');
+var addressField = document.querySelector('#address');
+
+pinHandle.addEventListener('mousedown', function (evt) {
+  evt.preventDefault();
+
+  var startCoords = {
+    x: evt.clientX,
+    y: evt.clientY
+  };
+
+  var mouseMoveHandler = function (moveEvt) {
+    moveEvt.preventDefault();
+
+    var shift = {
+      x: startCoords.x - moveEvt.clientX,
+      y: startCoords.y - moveEvt.clientY
+    };
+
+    startCoords = {
+      x: moveEvt.clientX,
+      y: moveEvt.clientY
+    };
+
+    pinHandle.style.top = (pinHandle.offsetTop - shift.y) + 'px';
+    pinHandle.style.left = (pinHandle.offsetLeft - shift.x) + 'px';
+    addressField.value = 'x: ' + ((pinHandle.offsetTop - shift.y) - pinHandle.clientHeight) + 'px, y: ' + Math.floor((pinHandle.offsetLeft - shift.x) - pinHandle.clientWidth / 2) + ' px';
+  };
+  var mouseUpHandler = function (upEvt) {
+    upEvt.preventDefault();
+
+    document.removeEventListener('mousemove', mouseMoveHandler);
+    document.removeEventListener('mouseup', mouseUpHandler);
+  };
+
+  document.addEventListener('mousemove', mouseMoveHandler);
+  document.addEventListener('mouseup', mouseUpHandler);
+});
